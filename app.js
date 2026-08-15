985014a70242040dfeaec227e6f4805238968403
K (rementia) <1992kirby427@gmail.com>
2026-08-15 14:56:43 +0900

    Revert "Fallback from empty mark modes on startup"
    This reverts commit 396e2ce6020348a654562bdb76d6f52c17647a71.

--- app.js
+++ app.js
@@ -870,17 +870,11 @@ function resetWordDataAfterSignIn() {
 async function loadCurrentModeAfterSignIn() {
   if (currentMode === "favorites") {
     await handleLoadFavoritesMode();
-    if (words.length === 0) {
-      await loadSheet(currentVol);
-    }
     return;
   }
 
   if (currentMode === "difficults") {
     await handleLoadDifficultsMode();
-    if (words.length === 0) {
-      await loadSheet(currentVol);
-    }
     return;
   }
 
