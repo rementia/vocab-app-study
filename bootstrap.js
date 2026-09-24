495c28766e333cd7ddf63b706daf4a4981b533e6
K <163677484+rementia@users.noreply.github.com>
2026-09-24 09:46:46 +0900

    Refresh short-tap click fix

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 import { init, finishInitialLoading } from './app.js?v=20260924-8';
-import { initMultipleChoiceLongPressEtymology } from './multipleChoiceLongPress.js?v=20260924-10';
+import { initMultipleChoiceLongPressEtymology } from './multipleChoiceLongPress.js?v=20260924-11';
 import { initMorphemeDoubleTapToggle } from './morphemeDoubleTapToggle.js?v=20260827-1';
 
 function loadSidebarLayoutStyles() {
