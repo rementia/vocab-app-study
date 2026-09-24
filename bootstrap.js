b00f5458fa06a510b32b4e84d3ae2b73ad8753f3
K <163677484+rementia@users.noreply.github.com>
2026-09-24 09:03:23 +0900

    Refresh direct choice etymology flow

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-import { init, finishInitialLoading } from './app.js?v=20260922-5';
-import { initMultipleChoiceLongPressEtymology } from './multipleChoiceLongPress.js?v=20260924-2';
+import { init, finishInitialLoading } from './app.js?v=20260924-3';
+import { initMultipleChoiceLongPressEtymology } from './multipleChoiceLongPress.js?v=20260924-3';
 import { initMorphemeDoubleTapToggle } from './morphemeDoubleTapToggle.js?v=20260827-1';
 
 function loadSidebarLayoutStyles() {
