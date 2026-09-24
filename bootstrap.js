9e4389becb08da90a68a993cf902b57aed4bc466
K <163677484+rementia@users.noreply.github.com>
2026-09-25 08:27:10 +0900

    Bust app cache for mobile choice fix

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,4 @@
-import { init, finishInitialLoading } from './app.js?v=20260924-8';
+import { init, finishInitialLoading } from './app.js?v=20260925-1';
 import { initMultipleChoiceLongPressEtymology } from './multipleChoiceLongPress.js?v=20260924-13';
 import { initMorphemeDoubleTapToggle } from './morphemeDoubleTapToggle.js?v=20260827-1';
 
